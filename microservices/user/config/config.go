0a41a001a8b991dbb4dc9b760b046fea37f9807b
Yux77 <forbidbanoutlook@gmail.com>
2024-11-23 01:01:04 +0800

    预防性提交

--- microservices/user/config/config.go
+++ microservices/user/config/config.go
@@ -1,10 +1,12 @@
 package config
 
 import (
+	"os"
+
 	userCache "github.com/Yux77Yux/platform_backend/microservices/user/cache"
 	userMQ "github.com/Yux77Yux/platform_backend/microservices/user/messaging"
 	userDB "github.com/Yux77Yux/platform_backend/microservices/user/repository"
-	"os"
+	service "github.com/Yux77Yux/platform_backend/microservices/user/service"
 )
 
 const (
@@ -14,14 +16,19 @@ const (
 	RABBITMQ_STR = "amqp://yuxyuxx:yuxyuxx@127.0.0.1:5672"
 
 	REDIS_STR = "redis://127.0.0.1:16379"
+
+	GRPC_SERVER_ADDRESS = ":50020"
 )
 
 var REDIS_PASSWORD string = os.Getenv("REDIS_PASSWORD")
 
 func init() {
+	service.InitStr(GRPC_SERVER_ADDRESS)
 	userMQ.InitStr(RABBITMQ_STR)
 	userCache.InitStr(REDIS_STR, REDIS_PASSWORD)
 	userDB.InitStr(MYSQL_READER_STR, MYSQL_WRITER_STR)
 
+	userMQ.Init()
 	userDB.Init()
+	userCache.Init()
 }
